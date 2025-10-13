f04cb14f901f713cca9725abf3ee8d9a0faccd26
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-10-13 14:12:39 -0300

    Add project files.

---
+++ ExemplosLambdaExpression/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ExemplosLambdaExpression
+{
+    delegate int Operacao(int a, int b);
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var op = 1;
+            Operacao operacao = Soma;
+
+            switch (op)
+            {
+                case 1:
+                    operacao = Soma;
+                    break;
+
+                case 2:
+                    operacao = Sub;
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.WriteLine(operacao(10, 4));
+
+            // 1 - Delegates
+
+            // 2 - Métodos Anônimos
+
+            // 3 - Lambda Expression
+        }
+
+        public static int Soma(int a, int b)
+        { 
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ PrimeiraApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder();
+        var app = builder.Build();
+
+        // Endpoint - É um ponto de ACESSO/INTERAÇÃO com minha aplicação
+        app.MapGet("/score/{cpf:long}", HelloWorld); // Variáveis de Segmento
+        app.MapGet("/score/{qualquercoisa:alpha}", MetodoQueRespondeEssaRequest); // Variáveis de Segmento
+
+        app.Run();
+    }
+
+    private static async Task MetodoQueRespondeEssaRequest(HttpContext context, string qualquercoisa)
+    {
+        await context.Response.WriteAsync($"Recebi: {qualquercoisa}");
+    }
+
+    private static async Task HelloWorld(HttpContext context, string cpf)
+    {
+        int score = 0;
+
+        if (cpf == "987654321")
+            score = 1000;
+        else if (cpf == "123456789")
+            score = 200;
+
+        await context.Response.WriteAsync($"O Score do CPF {cpf} é: {score}");
+    }
+}
