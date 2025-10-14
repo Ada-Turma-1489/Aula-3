d42d449bfc7180bb891d121150efb6b7a9e578f5
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2025-10-14 20:01:46 -0300

    Primeiro Exemplo de Lambda

--- ExemplosLambdaExpression/Program.cs
+++ ExemplosLambdaExpression/Program.cs
@@ -1,48 +1,70 @@
-﻿using System.Diagnostics;
-using System.Runtime.InteropServices;
+﻿using System.Threading.Channels;
 
 namespace ExemplosLambdaExpression
 {
-    delegate int Operacao(int a, int b);
-
     public class Program
     {
-        static void Main(string[] args)
+        public delegate int Operacao(int numero1, int numero2);
+
+        static void Main(string[] args) // Entry Point
         {
-            var op = 1;
-            Operacao operacao = Soma;
+            Operacao variavel1 = Soma; // Qualquer coisa que retorne int e receba dois ints;
+            Operacao variavel2 = Subtracao; // Qualquer coisa que retorne int e receba dois ints;
 
-            switch (op)
+            // Executar a Soma
+            ExecutaOperacao(variavel1);
+
+            // Executar a Subtração
+            ExecutaOperacao(variavel2);
+
+            // Métodos Anônimos
+            ExecutaOperacao(delegate (int a, int b) // <- Método Sem Nome (inline)
             {
-                case 1:
-                    operacao = Soma;
-                    break;
+                return a + b;
+            }); // Open Closed Principle 
 
-                case 2:
-                    operacao = Sub;
-                    break;
+            // Lambda `entrada => saida`
+            ExecutaOperacao((int a, int b) => a + b);
+            ExecutaOperacao((int a, int b) => a - b);
+            ExecutaOperacao((int a, int b) => a / b);
+            ExecutaOperacao((int a, int b) => a * b);
+            ExecutaOperacao((int a, int b) => Math.Max(a, b));
+            ExecutaOperacao((int a, int b) => (int)Math.Pow(a, b));
+            ExecutaOperacao((int a, int b) => 
+            {
+                Console.WriteLine($"Executando o pow entre {a} e {b}");
 
-                default:
-                    break;
-            }
+                var resultado = (int)Math.Pow(a, b);
 
-            Console.WriteLine(operacao(10, 4));
+                Console.WriteLine($"O resultado é {resultado}");
 
-            // 1 - Delegates
-
-            // 2 - Métodos Anônimos
-
-            // 3 - Lambda Expression
+                return resultado;
+            });
+        }
+ 
+        public static void ExecutaOperacao(Operacao operacao)
+        {
+            var resultado = operacao(7, 3);
+            Console.WriteLine($"O resultado da Operação é: {resultado}");
         }
 
+        // Assinatura do Método -> Tipo de Retorno, Parâmetros (ordem e os tipos)
+        // Retorna Int, Recebe dois Ints
         public static int Soma(int a, int b)
-        { 
+        {
             return a + b;
         }
 
-        public static int Sub(int a, int b)
+        // Retorna Int, Recebe dois Ints
+        public static int Subtracao(int a, int b)
         {
             return a - b;
         }
     }
 }
+
+
+
+// 1 - Delegates
+// 2 - Métodos Anônimos
+// 3 - Lambda Expression
